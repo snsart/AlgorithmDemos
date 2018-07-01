c2a4d312c51e5a47054d58f6b89fc1894c4c4987
snsart <snsart@163.com>
2018-07-01 12:01:41 +0800

    打乱数组

---
+++ demo4-shuffle/main.js
@@ -0,0 +1,19 @@
+/*
+* 打乱数组[1,2,3,4,5,6,7,8,9,10,11,12,13]
+* */
+var arr=[1,2,3,4,5,6,7,8,9,10,11,12,13];
+shuffle(arr);
+console.log(arr);
+function shuffle(arr){
+	for(var i=arr.length-1;i>0;i--){
+		var j=Math.floor(Math.random()*i);
+		if(j!=i){
+			var temp=arr[i];
+			arr[i]=arr[j];
+			arr[j]=temp;
+		}
+	}
+}
+
+
+
